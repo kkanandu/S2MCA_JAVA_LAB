a0d5a77f506b34302cd3303dc81235a28a64ef98
Anandu kk <116473698+kkanandu@users.noreply.github.com>
2023-07-14 18:24:52 +0530

    Update string sorting.java

--- CO2/string sorting.java
+++ CO2/string sorting.java
@@ -1,29 +1,23 @@
-import java.util.Arrays;
 import java.util.Scanner;
-public class StringSort 
-{
-public static void main(String[] args) 
-{
-Scanner input = new Scanner(System.in);
-System.out.print("Enter the number of strings: ");
-int a = input.nextInt();
-input.nextLine(); 
-String[] names = new String[a];
-for (int i = 0; i < a; i++) 
-{
-System.out.print("Enter string" + (i + 1) + ": ");
-names[i] = input.nextLine();
-}
-System.out.println("\nBefore sorting:");
-for (String name : names) 
-{
-System.out.println(name);
-}
-Arrays.sort(names);
-System.out.println("\nAfter sorting:");
-for (String name : names) 
-{
-System.out.println(name);
-}
-}
+class sort{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        char arr[]=new char[20];
+        System.out.println("Enter a string:");
+        String str=sc.next();
+        int l=str.length();
+        arr=str.toCharArray();
+        char temp;
+        for(int i=0;i<l;i++){
+            for(int j=i;j<l;j++){
+                if(arr[i]>arr[j]){
+                    temp=arr[j];
+                    arr[j]=arr[i];
+                    arr[i]=temp;
+                }
+            }
+        }
+        System.out.println(str+" after sorting:");
+        System.out.println(arr);
+    }
 }
